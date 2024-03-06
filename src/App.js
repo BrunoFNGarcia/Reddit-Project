722caa9d09b50f24cc95aa16cbe4a25426181f82
Bruno <129994097+BrunoFNGarcia@users.noreply.github.com>
2024-03-05 23:34:40 -0300

    Fix Gallery.js

--- src/App.js
+++ src/App.js
@@ -14,45 +14,41 @@ function App() {
 
   useEffect(() => {
     const fetchData = async () => {
-      if (selectedSubreddit === '') {
-        const response = await fetch('https://api.reddit.com/.json?feed=home');
-        const json = await response.json();
-        setFeed(json.data.children);
-      } else {
-        const response = await fetch(`https://api.reddit.com/r/${selectedSubreddit}/.json`);
-        const json = await response.json();
-        setFeed(json.data.children);
-      };
+      try {
+        if (selectedSubreddit === '') {
+          const response = await fetch('https://api.reddit.com/.json?feed=home');
+          const json = await response.json();
+          setFeed(json.data.children);
+        } else {
+          const response = await fetch(`https://api.reddit.com/r/${selectedSubreddit}/.json`);
+          const json = await response.json();
+          setFeed(json.data.children);
+        };
+      } catch(error) {
+        console.error('Error fetching data:', error);
+      }
     };
     fetchData();
-  }, [selectedSubreddit])
-
-  // useEffect(() => {
-  //   if (selectedSubreddit === '') {
-  //     fetch('https://www.reddit.com/.json?feed=home')
-  //       .then(response => response.json())
-  //       .then(json => setFeed(json.data.children));
-  //   } else {
-  //     fetch(`https://www.reddit.com/r/${selectedSubreddit}/.json`)
-  //       .then(response => response.json())
-  //       .then(json => setFeed(json.data.children));
-  //   }
-  // }, [selectedSubreddit]);
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const response = await fetch('https://www.reddit.com/hot.json');
-  //     const json = await response.json();
-  //     setSubreddits(json.data.children);
-  //   };
-  //   fetchData();
-  // }, [subreddits])
+  }, [selectedSubreddit]);
 
   useEffect(() => {
-    fetch('https://www.reddit.com/hot.json')
-      .then(response => response.json())
-      .then(json => setSubreddits(json.data.children))
-  }, []);
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://api.reddit.com/hot.json');
+        const json = await response.json();
+        setSubreddits(json.data.children);
+      } catch(error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    fetchData();
+  }, [])
+
+  // useEffect(() => {
+  //   fetch('https://www.reddit.com/hot.json')
+  //     .then(response => response.json())
+  //     .then(json => setSubreddits(json.data.children))
+  // }, []);
 
   const handleClick = (value) => {
     setSelectedSubreddit(value);
--- src/Posts/components/gallery/Gallery.js
+++ src/Posts/components/gallery/Gallery.js
@@ -10,32 +10,43 @@
 //     )
 // })}
 
+import './Gallery.css';
 import { useState } from "react";
 
 function Gallery(props) {
-    const imagesUrls = props.keys.map(key => {
-        props.gallery[key].s.u.split('?')[0].replace('preview', 'i')
-    });
+    const imagesUrls = props.keys.map(key => props.gallery[key].s.u.split('?')[0].replace('preview', 'i'));
+    // const imagesUrls = props.keys.map(key => props.gallery[key].s.u.split('?')[0].replace('preview', 'i'));
     const [currentUrl, setCurrentUrl] = useState([0, imagesUrls[0]]);
+    // props.keys.map(key => console.log(props.gallery[key].s.u.split('?')[0].replace('preview', 'i')));
+    // console.log(currentUrl);
 
     return (
         <div>
-            {currentUrl[0] < imagesUrls.length - 1 &&
-                <img 
-                    className="RightArrow"
-                    onClick={() => setCurrentUrl(prev => [prev[0] + 1, imagesUrls[prev[0] + 1]])}
-                    src='../../../Assets/angle-circle-right-icon.png'
-                    alt='next image arrow'
-                />
+            {currentUrl[0] > 0 &&
+                <svg 
+                    id='LeftArrow'
+                    onClick={() => setCurrentUrl(prev => [prev[0] - 1, imagesUrls[prev[0] - 1]])}
+                    xmlns="http://www.w3.org/2000/svg" 
+                    fill="currentColor" 
+                    class="bi bi-arrow-right-circle" 
+                    viewBox="0 0 16 16"
+                    alt='previous image arrow'
+                >
+                    <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+                </svg>
             }
-            {
-                currentUrl[0] > 0 &&
-                    <img
-                        className="LeftArrow"
-                        onClick={() => setCurrentUrl(prev => [prev[0] - 1, imagesUrls[prev[0] - 1]])}
-                        src='../../../Assets/angle-circle-right-icon.png'
-                        alt='previous image arrow' 
-                    />
+            {currentUrl[0] < imagesUrls.length - 1 &&
+                <svg 
+                    id='RightArrow'
+                    onClick={() => setCurrentUrl(prev => [prev[0] + 1, imagesUrls[prev[0] + 1]])}
+                    xmlns="http://www.w3.org/2000/svg" 
+                    fill="currentColor" 
+                    class="bi bi-arrow-right-circle" 
+                    viewBox="0 0 16 16"
+                    alt='next image arrow'
+                >
+                    <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+                </svg>
             }
             <img 
                 className="GalleryImage"
