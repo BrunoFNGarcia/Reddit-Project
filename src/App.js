8df153f835cb47f5fae5f75a7f745ddfea5a3d3e
Bruno <129994097+BrunoFNGarcia@users.noreply.github.com>
2024-11-04 23:53:59 -0300

    Minor changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { ReactComponent as RedditLogo } from './Assets/RedditLogo.svg';
 import SearchBar from './SearchBar/SearchBar';
